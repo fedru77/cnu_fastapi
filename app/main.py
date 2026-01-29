18f943fc1f679878aac5a02d2f622248c8f36153
fedru77 <fdffdf1029@gmail.com>
2026-01-29 14:48:38 +0900

    Update main.py and dependencies

--- app/main.py
+++ app/main.py
@@ -1,21 +1,35 @@
-from fastapi import FastAPI
-
-from app.week2.practice1 import router as practice1_router
-from app.week2.practice2 import router as practice2_router
-from app.week2.practice3 import router as practice3_router
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+items = [
+    {'id':1, 'data':'apple', 'color':'red'},
+    {'id':2, 'data':'banana', 'color':'yellow'},
+    {'id':3, 'data':'cherry', 'color':'red'}
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class ItemCreate(BaseModel):
+    data: str
+    color: str
 
+@app.post('/items')
+def create_item(payload: ItemCreate):
+    item = {'id': len(items)+1, **payload.model_dump()}
+    items.append(item)
+    return item
 
-@app.post("/items/{item}")
-async def create_item(item: str):
-    return {"item": item}
+@app.get('/items/{item_id}')
+def get_item_by_id(item_id: int):
+    for item in items:
+        if item.get('id') == item_id:
+            return item
+    raise HTTPException(status_code=404, detail='Item not found')
 
-app.include_router(practice1_router)
-app.include_router(practice2_router)
-app.include_router(practice3_router)
+@app.get('/items')
+def get_items_by_color(color: Optional[str] = None):
+    results = [item for item in items if item.get('color') == color]
+    if results: 
+        return {"data": results}
+    raise HTTPException(status_code=404, detail='Item not found')
